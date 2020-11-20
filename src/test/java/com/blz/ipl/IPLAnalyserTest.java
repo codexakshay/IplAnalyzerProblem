cc1c986eb7b625f7c55c764b992cbe457aef4dac
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-11-20 13:19:08 +0530

    IplAnalyzerProblem : Finding Batsman with Max Average And Strike Rate

---
+++ src/test/java/com/blz/ipl/IPLAnalyserTest.java
@@ -0,0 +1,79 @@
+package com.blz.ipl;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.google.gson.Gson;
+
+public class IPLAnalyserTest {
+
+	private static final String RIGHT_MOST_RUNS_CSV = "F:\\BridgeLabz Fellowship Program\\practice\\IPLAnalyserProblem\\src\\test\\resources\\MostRuns.csv";
+	private static final double DELTA = 1e-15;
+
+	static IPLAnalyser iplbatting;
+
+	@BeforeClass
+	public static void obj() {
+		iplbatting = new IPLAnalyser();
+	}
+
+	@Test
+	public void givenMostRunsCSV_ShouldReturnNumberOfRecords() throws IPLAnalyserException {
+		Assert.assertEquals(101, iplbatting.loadCSVData(RIGHT_MOST_RUNS_CSV));
+	}
+
+	@Test
+	public void givenMostRunsCSV_ShouldReturnTopBattingAverages() {
+		try {
+			iplbatting.loadCSVData(RIGHT_MOST_RUNS_CSV);
+			String sortedData = iplbatting.getTopBattingAverages();
+			IPLBattingAnalysis[] censusCSV = new Gson().fromJson(sortedData, IPLBattingAnalysis[].class);
+			Assert.assertEquals(83.2, censusCSV[censusCSV.length - 1].avg, DELTA);
+		} catch (IPLAnalyserException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void givenMostRunsCSV_ShouldReturnTopStrikingRatesOfBatsman() {
+		try {
+			iplbatting.loadCSVData(RIGHT_MOST_RUNS_CSV);
+			String sortedData = iplbatting.getTopStrikingRates();
+			IPLBattingAnalysis[] mostRunsCSV = new Gson().fromJson(sortedData, IPLBattingAnalysis[].class);
+			Assert.assertEquals(333.33, mostRunsCSV[mostRunsCSV.length - 1].avg, DELTA);
+		} catch (IPLAnalyserException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void givenMostRunsCSV_ShouldReturnMax4sCricketer() throws IOException, IPLAnalyserException {
+		List<IPLBattingAnalysis> lst = new IPLAnalyser().getTopFoursHittingBatsman(RIGHT_MOST_RUNS_CSV);
+		Assert.assertEquals("Shikhar Dhawan",
+				new IPLAnalyser().getTopFoursHittingBatsman(RIGHT_MOST_RUNS_CSV).get(0).playerName);
+	}
+
+	@Test
+	public void givenMostRunsCSV_ShouldReturnMax6sCricketer() throws IOException, IPLAnalyserException {
+		List<IPLBattingAnalysis> lst = new IPLAnalyser().getTopSixesHittingBatsman(RIGHT_MOST_RUNS_CSV);
+		Assert.assertEquals("Andre Russell",
+				new IPLAnalyser().getTopSixesHittingBatsman(RIGHT_MOST_RUNS_CSV).get(0).playerName);
+	}
+
+	@Test
+	public void givenMostRunsCSV_ShouldReturnBestStrikingRates_With6sAnd4s() throws IOException, IPLAnalyserException {
+		Assert.assertEquals("Andre Russell",
+				new IPLAnalyser().getBestStrikingRatesWithBoundaries(RIGHT_MOST_RUNS_CSV).get(0).playerName);
+	}
+
+	@Test
+	public void givenMostRunsCSV_ShouldReturnbestStrikingRatesWithBestAverages()
+			throws IOException, IPLAnalyserException {
+		Assert.assertEquals("MS Dhoni",
+				new IPLAnalyser().getGreatAveragesWithBestStrikingRates(RIGHT_MOST_RUNS_CSV).get(0).playerName);
+	}
+}
