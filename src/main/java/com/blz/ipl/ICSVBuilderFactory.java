c78f66e83d9cf685727c06bc80af9979b5ba07bc
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-11-20 13:24:27 +0530

    IplAnalyzerProblem : Finding Bowler with Top Bowling Average

---
+++ src/main/java/com/blz/ipl/ICSVBuilderFactory.java
@@ -0,0 +1,9 @@
+package com.blz.ipl;
+
+import java.io.Reader;
+import java.util.List;
+
+public interface ICSVBuilderFactory<E> {
+
+	List<E> getCSVFileList(Reader reader, Class<E> csvClass) throws IPLAnalyserException;
+}
