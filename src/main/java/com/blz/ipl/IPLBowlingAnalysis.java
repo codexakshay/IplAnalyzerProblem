5cea9bf13eebca4bef6960c8a79eab5afb1ab082
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-11-20 13:26:49 +0530

    IplAnalyzerProblem : Finding Bowler with Top Bowling Strike Rate

---
+++ src/main/java/com/blz/ipl/IPLBowlingAnalysis.java
@@ -0,0 +1,53 @@
+package com.blz.ipl;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class IPLBowlingAnalysis {
+
+	@CsvBindByName(column = "POS", required = true)
+	public int position;
+
+	@CsvBindByName(column = "PLAYER", required = true)
+	public String playerName;
+
+	@CsvBindByName(column = "Mat", required = true)
+	public int matches;
+
+	@CsvBindByName(column = "Inns", required = true)
+	public int innings;
+
+	@CsvBindByName(column = "Ov", required = true)
+	public double overs;
+
+	@CsvBindByName(column = "Runs", required = true)
+	public int runs;
+
+	@CsvBindByName(column = "Wkts", required = true)
+	public int wickets;
+
+	@CsvBindByName(column = "BBI", required = true)
+	public int bbi;
+
+	@CsvBindByName(column = "Avg", required = true)
+	public double avg;
+
+	@CsvBindByName(column = "Econ", required = true)
+	public double economy;
+
+	@CsvBindByName(column = "SR", required = true)
+	public double strikeRate;
+
+	@CsvBindByName(column = "4w", required = true)
+	public int fourWkHaul;
+
+	@CsvBindByName(column = "5w", required = true)
+	public int fiveWkHaul;
+
+	@Override
+	public String toString() {
+		return "IPLBatting [position=" + position + ", player=" + playerName + ", match=" + matches + ", innings="
+				+ innings + ", Overs=" + overs + ", runs=" + runs + ", wickets" + wickets + ", average=" + avg
+				+ ", BBI=" + bbi + ", average=" + avg + ", economy=" + economy + " strikeRate=" + strikeRate
+				+ ", 4Wk-Hauls" + fourWkHaul + ", 5Wk-Hauls " + fiveWkHaul + " " + "]";
+	}
+}
